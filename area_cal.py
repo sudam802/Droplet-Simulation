b5635c3ce49851916b020f158c9e406884743fbc
sudam802 <57206830+sudam802@users.noreply.github.com>
2024-06-22 08:49:18 +0530

    Update area_cal.py

--- area_cal.py
+++ area_cal.py
@@ -1,1 +1,14 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
+
+# Read the image
+image = cv2.imread('final.png')[315:451,550:670]
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+plt.imshow(image)
+r=image[:,:,2]
+
+width=r.shape[0]
+height=r.shape[1]
+print(r.shape)
