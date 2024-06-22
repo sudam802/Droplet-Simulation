8506cc85b559cb16c8c9c47265985afad866c5fb
sudam802 <57206830+sudam802@users.noreply.github.com>
2024-06-22 08:50:24 +0530

    Update area_cal.py

--- area_cal.py
+++ area_cal.py
@@ -12,3 +12,19 @@ r=image[:,:,2]
 width=r.shape[0]
 height=r.shape[1]
 print(r.shape)
+count=0
+for x in range(r.shape[0]):
+  for y in range(r.shape[1]):
+    val=r[x,y]
+    if val>200:
+      count=count+1
+print(count)
+all=r.shape[0]*r.shape[1]
+potion=count/all
+print(all)
+
+h=0.8
+w=h*width/height
+all_area=h*w
+red_area=all_area*potion
+print(str(red_area)+'mm^2')
